af9ec5877f445c3adee3e15ec88c08d47fbc2276
BenE <158221658+kadlex-web@users.noreply.github.com>
2025-09-30 12:05:55 -0400

    wrote cleanInput function

--- main.go
+++ main.go
@@ -2,7 +2,20 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
+
+func cleanInput(text string) []string {
+	// Maybe change order of operations; lowercase first than use fields because fields trims?
+	// Step 1 -- trim all leading and trailing whitespace from text string
+	trimmedInput := strings.Trim(text)
+	// Step 2 -- lowercase text string
+	trimmedAndLoweredInput := strings.ToLower(trimmedInput)
+	// Step 3 -- split string into a slice of strings, where each item is a word within the original string
+	cleanedInput := strings.Fields(trimmedAndLoweredInput)
+	
+	return cleanedInput
+}
 
 func main() {
 	fmt.Println("Hello, World!")

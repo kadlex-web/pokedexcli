62cc868ad24ae51a5dad28f45d5d251bb2ecb128
kadlex <akulp18@gmail.com>
2025-09-30 18:40:04 -0400

    push

--- main.go
+++ main.go
@@ -4,6 +4,15 @@ import (
 	"fmt"
 )
 
+/*
+	Splits users words into a slice of strings based upon whitespace
+
+Hello world becomes ['hello', 'world] for example
+*/
+func cleanInput(text string) []string {
+	return ['word', 'word']
+}
+
 func main() {
 	fmt.Println("Hello, World!")
 }
---
+++ repl_test.go
@@ -0,0 +1,33 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:	"HELLO WORLD",
+			expected: []string{"hello", "world"},
+		}
+	}
+
+	for _, c := range cases {
+	actual := cleanInput(c.input)
+	// Check the length of the actual slice against the expected slice
+	// if they don't match, use t.Errorf to print an error message
+	// and fail the test
+	for i := range actual {
+		word := actual[i]
+		expectedWord := c.expected[i]
+		// Check each word in the slice
+		// if they don't match, use t.Errorf to print an error message
+		// and fail the test
+	}
+}
+}

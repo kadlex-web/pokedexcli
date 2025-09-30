8b9d43f9e5b4a5a3ff0d1502b6203547e231e251
kadlex <akulp18@gmail.com>
2025-09-30 19:25:48 -0400

    cleanInput func works, tests pass

--- main.go
+++ main.go
@@ -8,11 +8,12 @@ import (
 func cleanInput(text string) []string {
 	// Maybe change order of operations; lowercase first than use fields because fields trims?
 	// Step 1 -- trim all leading and trailing whitespace from text string
-	trimmedInput := strings.Trim(text)
+	trimmedInput := strings.Trim(text, " ")
 	// Step 2 -- lowercase text string
 	trimmedAndLoweredInput := strings.ToLower(trimmedInput)
 	// Step 3 -- split string into a slice of strings, where each item is a word within the original string
 	cleanedInput := strings.Fields(trimmedAndLoweredInput)
+	fmt.Println(cleanedInput)
 
 	return cleanedInput
 }

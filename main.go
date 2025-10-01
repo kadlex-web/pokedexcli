4693ec8bdbcf7a73c194b0c2c30f07013cbf9160
BenE <158221658+kadlex-web@users.noreply.github.com>
2025-10-01 16:05:51 -0400

    Update main.go

--- main.go
+++ main.go
@@ -7,6 +7,42 @@ import (
 	"strings"
 )
 
+// struct which defines behavior for a cliCommand struct (object)
+type cliCommand struct {
+	name string
+	description string
+	callback func() error
+}
+// map which defines supported commands 
+commands := map[string]cliCommand{
+	"exit": {
+		name: "exit",
+		description: "Exit the Pokedex",
+		callback: commandExit,
+	}
+}
+
+// Function which accepts the callback
+func ProcessCommand(command string) {
+	//takes the cleaned command input and sees if it's in the commands map
+	//if there's a match call the call back and return any errors
+	elem, ok := commands[command]
+	if ok {
+		commands[command][callback]
+	}
+	// if not just print Unknown command
+	else {
+		fmt.Println("Unknown command")
+	}
+}
+
+// callback for exit command
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
 func cleanInput(text string) []string {
 	// Maybe change order of operations; lowercase first than use fields because fields trims?
 	// Step 1 -- trim all leading and trailing whitespace from text string
@@ -24,7 +60,7 @@ func main() {
 
 	for scanner.Scan() {
 		input := cleanInput(scanner.Text())
-		fmt.Printf("Your command was: %s", input[0])
+		command := ProcessCommand(input[0])
 		fmt.Print("\nPokedex > ")
 	}
 }

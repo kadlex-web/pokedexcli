7b96fdee320af7cfced29c518a4361e1461df4c9
kadlex <akulp18@gmail.com>
2025-10-13 20:35:21 -0400

    added internal directory. created getLocations func v2

--- internal/getLocation.go
+++ internal/getLocation.go
@@ -6,6 +6,16 @@ import (
 	"net/http"
 )
 
+type locationJson struct {
+	Count    int    `json:"count"`
+	Next     string `json:"next"`
+	Previous any    `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
+
 func getLocations() error {
 	res, err := http.Get("https://pokeapi.co/api/v2/location-area/")
 	if err != nil {

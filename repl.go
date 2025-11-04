437c4df7db3c45ad802fb15cfb9bc4e56b739b77
BenE <158221658+kadlex-web@users.noreply.github.com>
2025-11-04 15:28:38 +0000

    rewrote commands variable as a getter function so that it can be iterated over

--- repl.go
+++ repl.go
@@ -13,27 +13,36 @@ type cliCommand struct {
 	description string
 	callback    func() error
 }
+
 // map which defines supported commands
-var commands = map[string]cliCommand{
-	"exit": {
-		name:        "exit",
-		description: "Exit the Pokedex",
-		callback:    commandExit,
-	},
-	"help": {
-		name:		"help",
-		description: "Displays a help message",
-		callback:	commandHelp,
-	},
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"map": {
+			name:        "map",
+			description: "Displays the name of 20 locations on the overworld",
+			callback:    commandMap,
+		},
+	}
 }
 
 // Function which accepts the callback
 func ProcessCommand(command string) {
 	//takes the cleaned command input and sees if it's in the commands map
 	//if there's a match call the call back and return any errors
+	commands := getCommands()
 	elem, ok := commands[command]
 	if ok {
-		// If the command exists in the commands map, execute it 
+		// If the command exists in the commands map, execute it
 		elem.callback()
 	} else {
 		// if not just print Unknown command
@@ -47,16 +56,31 @@ func commandExit() error {
 	os.Exit(0)
 	return nil
 }
-// callback for help command
-func commandHelp() error {
-	fmt.Print("Welcome to the Pokedex!\n")
-	fmt.Println("Usage:")
-	fmt.Println()
-	fmt.Println("help: Displays a help message")
-	fmt.Println("exit: Exit the Pokedex")
+
+func commandMap() error {
+	fmt.Println("Not implemented yet")
 	return nil
 }
 
+// callback for help command
+func commandHelp() error {
+	commands := getCommands()
+
+	fmt.Print("Welcome to the Pokedex!\n")
+	fmt.Println("Usage:")
+	fmt.Println()
+
+	for _, value := range commands {
+		fmt.Printf("%s: %s", value.name, value.description)
+		fmt.Println()
+	}
+
+	//fmt.Println("help: Displays a help message")
+	//fmt.Println("exit: Exit the Pokedex")
+	return nil
+}
+
+// Cleans the input and returns a []string of the cleaned inputs
 func cleanInput(text string) []string {
 	// Maybe change order of operations; lowercase first than use fields because fields trims?
 	// Step 1 -- trim all leading and trailing whitespace from text string
@@ -67,7 +91,6 @@ func cleanInput(text string) []string {
 	cleanedInput := strings.Fields(trimmedAndLoweredInput)
 	return cleanedInput
 }
-
 
 func startRepl() {
 	// need to gracefully handle if the user submits a command with no input
